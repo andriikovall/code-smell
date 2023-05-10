340dacf8e1c7134de9e59e8295c88ccbc154dce6
andriikovall <anderkoval3@gmail.com>
2023-05-10 21:30:45 +0300

    docs: added comments about task 2 refactoring

--- task2/old.ts
+++ task2/old.ts
@@ -9,6 +9,7 @@ enum TransportType {
 }
 
 class Transport {
+  // big class with many responsibilities of being a Car, Plane and Submarine at the same time
   private m_takeOffTime: number;
   private m_landingTime: number;
   private m_diveTime: number;
@@ -20,7 +21,10 @@ class Transport {
   }
 
   public getSpeed(distance: number, time: number): number {
+    // simplify the condition, remove unnecessary nesting
     if (time !== 0) {
+      // of course switch here breaks object oriented design, we should use polymorphism
+      // (Replace Type Code with Subclasses), (Replace Conditional with Polymorphism) is good as well
       switch (this.m_type) {
         case TransportType.eCar:
           return distance / time;
